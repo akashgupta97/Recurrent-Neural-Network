80fd299bfa237bcec12288e05d82b72c3e19fc5c
Akash <akash.darknight@gmail.com>
2017-09-30 13:03:00 +0530

    Recurrent_Neural_Networks

--- rnn.py
+++ rnn.py
@@ -57,4 +57,5 @@ parameters = {"Waa": Waa, "Wax": Wax, "Wya": Wya, "ba": ba, "by": by}
 
 a_next, yt_pred, cache = rnn_cell_forward(xt, a_prev, parameters)
 print("a_next[4] = ", a_next[4])
-print("a_next.shape = ", a_next.shape)
+print("a_next.shape = ", a_next.shape)print("yt_pred[1] =", yt_pred[1])
+print("yt_pred[1] =", yt_pred[1])
