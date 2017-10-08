6d3f033864bec3a17a2bb3d783ddd5e34920a293
Akash <akash.darknight@gmail.com>
2017-10-08 14:05:00 +0530

    Recurrent_Neural_Networks

--- rnn.py
+++ rnn.py
@@ -99,3 +99,16 @@ def rnn_forward(x, a0, parameters):
 
     # Initialize a_next (≈1 line)
     a_next = a0
+
+    # loop over all time-steps
+    for t in range(T_x):
+        # Update next hidden state, compute the prediction, get the cache (≈1 line)
+        a_next, yt_pred, cache = rnn_cell_forward(x[:, :, t], a_next, parameters)
+        # Save the value of the new "next" hidden state in a (≈1 line)
+        a[:, :, t] = a_next
+        # Save the value of the prediction in y (≈1 line)
+        y_pred[:, :, t] = yt_pred
+        # Append "cache" to "caches" (≈1 line)
+        caches.append(cache)
+
+        ### END CODE HERE ###
