774a9484d3c9ef825de78e2faa54e773a61fbd87
Akash <akash.darknight@gmail.com>
2017-10-20 14:05:00 +0530

    Recurrent_Neural_Networks

--- rnn.py
+++ rnn.py
@@ -143,3 +143,21 @@ print("y.shape = ", y.shape)
 print("caches[1][1[1]] =", caches[1][1][1])
 print("c[1][2][1]", c[1][2][1])
 print("len(caches) = ", len(caches))
+
+
+def rnn_cell_backward(da_next, cache):
+    """
+    Implements the backward pass for the RNN-cell (single time-step).
+
+    Arguments:
+    da_next -- Gradient of loss with respect to next hidden state
+    cache -- python dictionary containing useful values (output of rnn_cell_forward())
+
+    Returns:
+    gradients -- python dictionary containing:
+                        dx -- Gradients of input data, of shape (n_x, m)
+                        da_prev -- Gradients of previous hidden state, of shape (n_a, m)
+                        dWax -- Gradients of input-to-hidden weights, of shape (n_a, n_x)
+                        dWaa -- Gradients of hidden-to-hidden weights, of shape (n_a, n_a)
+                        dba -- Gradients of bias vector, of shape (n_a, 1)
+    """
